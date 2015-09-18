767654ed24249d0d01877c7452f5dbde7c7603c9
Jeisson Prieto <japrietov@unal.edu.c>
2015-09-17 21:08:11 -0500

    Clase Usuario

---
+++ User.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.lang.String;
+
+/**
+ *
+ * @author ManuelFelipe
+ */
+public class User {
+    public String name;
+    public String name;
+    public int age;
+    public String username;
+    public String password;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
