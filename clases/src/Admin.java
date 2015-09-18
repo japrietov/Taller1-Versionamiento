b5ab98a4208db503362139d65ff7eb61af31484d
Jeisson Prieto <japrietov@unal.edu.c>
2015-09-17 21:20:31 -0500

    Admin Class und Regular Class added

---
+++ clases/src/Admin.java
@@ -0,0 +1,23 @@
+/**
+ * Created by japrietov on 17/09/15.
+ */
+public class Admin {
+    public int level;
+    public double rating;
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+}
---
+++ clases/src/Regular.java
@@ -0,0 +1,33 @@
+/**
+ * Created by japrietov on 17/09/15.
+ */
+public class Regular {
+
+    public int postViews;
+    public int strikesNumber;
+    public int starsNumber;
+
+    public int getPostViews() {
+        return postViews;
+    }
+
+    public void setPostViews(int postViews) {
+        this.postViews = postViews;
+    }
+
+    public int getStrikesNumber() {
+        return strikesNumber;
+    }
+
+    public void setStrikesNumber(int strikesNumber) {
+        this.strikesNumber = strikesNumber;
+    }
+
+    public int getStarsNumber() {
+        return starsNumber;
+    }
+
+    public void setStarsNumber(int starsNumber) {
+        this.starsNumber = starsNumber;
+    }
+}
